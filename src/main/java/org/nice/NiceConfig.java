72e1baeaded9d3fcf071a8a201826a8042187af6
jayuwu97 <jasony25029716@gmail.com>
2026-09-21 20:14:40 +1000

    Update NiceConfig.java

--- src/main/java/org/nice/NiceConfig.java
+++ src/main/java/org/nice/NiceConfig.java
@@ -8,9 +8,21 @@ import net.runelite.client.config.ConfigItem;
 public interface NiceConfig extends Config
 {
 	@ConfigItem(
+			keyName = "screenshot",
+			name = "Screenshot",
+			description = "Take a screenshot whenever you deal 69 damage",
+			position = 0
+	)
+	default boolean screenshot()
+	{
+		return true;
+	}
+
+	@ConfigItem(
 			keyName = "overheadText",
 			name = "Show overhead text",
-			description = "Show 'Nice.' above your character when you deal 69 damage"
+			description = "Show 'Nice.' above your character whenever you deal 69 damage",
+			position = 1
 	)
 	default boolean overheadText()
 	{
@@ -20,7 +32,8 @@ public interface NiceConfig extends Config
 	@ConfigItem(
 			keyName = "chatMessage",
 			name = "Show chat message",
-			description = "Show 'Nice.' in the chatbox when you deal 69 damage"
+			description = "Show 'Nice.' in the chatbox whenever you deal 69 damage",
+			position = 2
 	)
 	default boolean chatMessage()
 	{
